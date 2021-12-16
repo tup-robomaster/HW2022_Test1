85c4246fb84948b03b8ac7f403c38e5fc408170c
push-rush <92003922+push-rush@users.noreply.github.com>
2021-12-16 14:48:05 +0800

    Add files via upload

---
+++ LiuBiao/armor_detect.cpp
@@ -0,0 +1,428 @@
+#include "armor_detect.h"
+
+// 计算两个点之间的距离
+double calc_distance(Point2f p1, Point2f p2)
+{
+    return pow(pow(p1.x-p2.x,2)+pow(p1.y-p2.y,2),0.5);
+}
+
+Mat bright_adjust(Mat frame)
+{
+    Mat dstImage  = frame;
+    int contrast = 20, bright = 20;
+    for (int i = 0; i < frame.rows; i++)
+    {
+        for (int j = 0; j < frame.cols; j++)
+        {
+            //也可以用for循环
+            dstImage.at<Vec3b>(i, j)[0] = saturate_cast<uchar>(frame.at<Vec3b>(i, j)[0] * contrast*0.01 + bright);
+            dstImage.at<Vec3b>(i, j)[1] = saturate_cast<uchar>(frame.at<Vec3b>(i, j)[1] * contrast*0.01 + bright);
+            dstImage.at<Vec3b>(i, j)[2] = saturate_cast<uchar>(frame.at<Vec3b>(i, j)[2] * contrast*0.025 + bright);
+        }
+    }
+    // frame.convertTo(dstImage, -1, 0.01, 20);
+    return dstImage;
+}
+
+Mat white_balance(Mat frame)
+{
+    Mat frame1 = frame;
+    vector<Mat>rgb;
+    vector<int> a(2, 0);
+	//RGB三通道分离
+	split(frame, rgb);
+	a[0] = 1;
+	a[1] = 2;
+	//求原始图像的RGB分量的均值
+	double R, G, B;
+	//B = mean(rgb[0])[0];
+ 
+	G = mean(rgb[1])[0];
+	R = mean(rgb[2])[0];
+	Scalar sca = mean(rgb[0]);
+	B = sca[0];
+	//需要调整的RGB分量的增益
+	double KR, KG, KB;
+	KB = (R + G + B) / (3 * B);
+	KG = (R + G + B) / (3 * G);
+	KR = (R + G + B) / (3 * R);
+ 
+	//调整RGB三个通道各自的值
+	rgb[0] = rgb[0] * KB;
+	rgb[1] = rgb[1] * KG;
+	rgb[2] = rgb[2] * KR;
+ 
+	//RGB三通道图像合并
+	merge(rgb, frame1);
+	// imshow("白平衡", frame);
+    return frame1;
+}
+
+armor::armor(){}
+
+armor::armor(const LED_Stick& L1, const LED_Stick& L2){
+    Led_stick[0]= L1;
+    Led_stick[1]= L2;
+    error_angle = fabs(L1.rect.angle - L2.rect.angle);
+
+    rect.width = abs(static_cast<int>(L1.rect.center.x - L2.rect.center.x));
+    rect.height = static_cast<int>((L1.rect.size.height + L1.rect.size.height)/2);
+    center.x = static_cast<int>((L1.rect.center.x + L2.rect.center.x)/2);
+    center.y = static_cast<int>((L1.rect.center.y + L2.rect.center.y)/2);
+    rect.x = center.x - rect.width/3;
+    rect.y = center.y - rect.height/3;
+    rect.width*= 2.0/3;
+    rect.height*= 2.0/3;
+}
+
+void armor::draw_rect( Mat& img, Point2f roi_offset_point) const
+{
+    rectangle(img, rect + Point_<int>(roi_offset_point), Scalar(255,255,255), 1);
+}
+
+void armor::draw_spot(Mat &img, Point2f roi_offset_point) const
+{
+    circle(img, center + Point_<int>(roi_offset_point), int(rect.height/4), Scalar(0,0,255), -1);
+}
+
+
+int armor::get_average_intensity(const Mat& img) {
+    if(rect.width < 1 || rect.height < 1 || rect.x < 1 || rect.y < 1
+            || rect.width + rect.x > img.cols || rect.height + rect.y > img.rows)
+        return 255;
+    Mat roi = img(Range(rect.y, rect.y + rect.height), Range(rect.x, rect.x + rect.width) );
+    //        imshow("roi ", roi);
+    average_intensity = static_cast<int>(mean(roi).val[0]);
+    return average_intensity;
+}
+
+
+void armor::max_match(vector<LED_Stick>& LED,size_t i,size_t j){
+    RotatedRect R, L;
+    if(Led_stick[0].rect.center.x > Led_stick[1].rect.center.x)
+    {
+        R = Led_stick[0].rect;
+        L = Led_stick[1].rect;
+    }else
+    {
+        R = Led_stick[1].rect;
+        L = Led_stick[0].rect;
+    }
+
+    float angle_8 = L.angle - R.angle;
+    if(angle_8 < 1e-3f) angle_8 = 0.0f;
+    float f = error_angle + 0.5 * angle_8;
+
+    if(!LED.at(i).matched && !LED.at(j).matched ) //容器最初无配对的等条，找到两个就直接配对 （matched = ture）
+    {
+        LED.at(i).matched = true;
+        LED.at(i).match_index = j;
+        LED.at(j).matched = true;
+        LED.at(j).match_index = i;
+        LED.at(i).match_factor = f;
+        LED.at(j).match_factor = f;
+    }
+    if(LED.at(i).matched && !LED.at(j).matched) //其中一个灯条有配对的灯条，另一个没有
+                                                //如果当前两个的灯条拟合出的旋转矩形的误差角度小于之前配对的误差角度
+                                                //当前两个灯条配对成功，同时之前有配对的灯条的配对灯条改为未配对状态（matched = false)
+    {
+        if(f < LED.at(i).match_factor)
+        {
+            LED.at(LED.at(i).match_index).matched = false;
+            LED.at(i).match_factor = f;
+            LED.at(i).match_index = j;
+            LED.at(j).matched = true;
+            LED.at(j).match_factor = f;
+            LED.at(j).match_index = i;
+
+        }
+    }
+    if(LED.at(j).matched && !LED.at(i).matched)
+    {
+        if(f < LED.at(j).match_factor )
+        {
+            LED.at(LED.at(j).match_index).matched = false;
+            LED.at(j).match_factor = f;
+            LED.at(j).match_index = i;
+            LED.at(i).matched = true;
+            LED.at(i).match_factor = f;
+            LED.at(i).match_index = j;
+        }
+    }
+    if(LED.at(j).matched && LED.at(i).matched
+            && LED.at(i).match_factor > f && LED.at(j).match_factor > f)
+    {
+        LED.at(LED.at(j).match_index).matched = false;
+        LED.at(LED.at(i).match_index).matched = false;
+        LED.at(i).matched = true;
+        LED.at(i).match_factor = f;
+        LED.at(i).match_index = j;
+        LED.at(j).matched = true;
+        LED.at(j).match_factor = f;
+        LED.at(j).match_index = i;
+    }
+}
+
+bool armor::is_suitable_size(void) const
+{
+    // 两个灯条体型相似
+    if(Led_stick[0].rect.size.height*0.7f < Led_stick[1].rect.size.height
+            && Led_stick[0].rect.size.height*1.3f > Led_stick[1].rect.size.height)
+    {
+        float armor_width = fabs(Led_stick[0].rect.center.x - Led_stick[1].rect.center.x);
+        if(armor_width > Led_stick[0].rect.size.width
+                && armor_width > Led_stick[1].rect.size.width
+                && armor_width > (Led_stick[0].rect.size.width+Led_stick[1].rect.size.width)*3)
+        {
+            float h_max = (Led_stick[0].rect.size.height + Led_stick[1].rect.size.height)/2.0f;
+            // 两个灯条高度差不大
+            if(fabs(Led_stick[0].rect.center.y - Led_stick[1].rect.center.y) < 0.8f* h_max )
+            {
+                // 长宽比判断
+                if(h_max*4.0f > rect.width && h_max < 1.2f* rect.width)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+Rect ArmorDetector::GetRoi(const Mat &img)
+{
+    Size img_size = img.size();
+    Rect rect_tmp = last_target_;
+    Rect rect_roi;
+    if(rect_tmp.x == 0 || rect_tmp.y == 0
+            || rect_tmp.width == 0 || rect_tmp.height == 0
+            || lost_cnt_ >= 15 || detect_cnt_%100 == 0
+        #ifndef FORCE_CHANGE_CAMERA
+            || update_cap_cnt < 20
+        #endif
+            )
+    {
+        last_target_ = Rect(0,0,img_size.width, img_size.height);
+        rect_roi = Rect(0,0,img_size.width, img_size.height);
+        return rect_roi;
+    }
+    else
+    {
+        float scale = 2;
+        if (lost_cnt_ < 30)
+            scale = 3;
+        else if(lost_cnt_ <= 60)
+            scale = 4;
+        else if(lost_cnt_ <= 120)
+            scale = 5;
+
+        int w = int(rect_tmp.width * scale);
+        int h = int(rect_tmp.height * scale);
+        int x = int(rect_tmp.x - (w - rect_tmp.width)*0.5f);
+        int y = int(rect_tmp.y - (h - rect_tmp.height)*0.5f);
+
+        rect_roi = Rect(x, y, w, h);
+
+        if(makeRectSafe(rect_roi, img_size)== false)
+        {
+            rect_roi = Rect(0,0,img_size.width, img_size.height);
+        }
+    }
+    return rect_roi;
+}
+
+bool ArmorDetector::DetectArmor(Mat &img, Rect roi_rect)
+{
+    // **预处理** -图像进行相应颜色的二值化
+    Mat roi_image = img(roi_rect);
+    Point2f offset_roi_point(roi_rect.x, roi_rect.y);
+    vector<LED_Stick> LED_Stick_v;  //用于存储找到的可能的灯条
+    Mat binary_brightness_img, binary_color_img, gray;
+    cvtColor(roi_image,gray,COLOR_BGR2GRAY);
+    
+    vector<Mat> bgr;
+    split(roi_image, bgr);
+    Mat result_img;
+    if(color_ == 0)
+    {
+        subtract(bgr[2], bgr[1], result_img);
+    }else
+    {
+        subtract(bgr[0], bgr[2], result_img);
+    }
+
+    threshold(gray, binary_brightness_img, gray_th_, 255, THRESH_BINARY);
+    threshold(result_img, binary_color_img, color_th_, 255, THRESH_BINARY);
+
+    // **提取可能的灯条** -利用灯条（灰度）周围有相应颜色的光圈包围
+#ifdef SHOW_BINARY_IMAGE
+    imshow("binary_brightness_img", binary_brightness_img);
+    imshow("binary_color_img", binary_color_img);
+#endif
+
+    vector<vector<Point>> contours_light;
+    vector<vector<Point>> contours_brightness;
+    findContours(binary_color_img, contours_light, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    findContours(binary_brightness_img, contours_brightness, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    
+    for(size_t i = 0; i < contours_brightness.size(); i++)
+    {
+        double area = contourArea(contours_brightness[i]);
+        if (area < 5.0 || 1e5 < area) continue;                 //第一层筛选：面积在一定范围
+        for(size_t ii = 0; ii < contours_light.size(); ii++)
+        {
+            if(pointPolygonTest(contours_light[ii], contours_brightness[i][0], false) >= 0.0 )
+            {
+                double length = arcLength(contours_brightness[i], true); // 第二层：灯条周长在一定范围
+                if (length > 5 && length <4000)
+                {          
+                    RotatedRect RRect = fitEllipse(contours_brightness[i]); //椭圆拟合效果更好
+#ifdef SHOW_LIGHT_CONTOURS
+                    // 旋转矩形提取四个点
+                    Point2f rect_point[4];
+                    RRect.points(rect_point);
+                    for (int i = 0; i < 4 ; i++)
+                    {
+                        line(img, rect_point[i]+offset_roi_point, rect_point[(i+1)%4]+offset_roi_point, Scalar(255,0,255),1);
+                    }
+#endif
+                    // 角度换算，将拟合椭圆0~360 -> -180~180
+                    if(RRect.angle>90.0f)
+                        RRect.angle =  RRect.angle - 180.0f;
+#ifdef SHOW_LIGHT_PUT_TEXT
+                    putText(img, to_string(RRect.angle), RRect.center + Point2f(2,2) + offset_roi_point, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255,255,255), 1);
+#endif
+                    if (fabs(RRect.angle) <= 30)  // 第三层筛选：超过一定角度的灯条不要
+                    {
+                        LED_Stick r(RRect);
+                        LED_Stick_v.push_back(r);
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    // **寻找可能的装甲板** -遍历每个可能的灯条, 两两灯条拟合成装甲板进行逻辑判断
+    for(size_t i = 0; i < LED_Stick_v.size() ; i++)
+    {
+        for(size_t j = i + 1; j < LED_Stick_v.size() ; j++)
+        {
+            armor arm_tmp( LED_Stick_v.at(i), LED_Stick_v.at(j) );
+            if (arm_tmp.error_angle < 8.0f)     //第四层筛选：两等条误差角度小于某一值
+            {
+
+#ifdef SHOW_ARMOR_PUT_TEXT
+                putText(img, to_string(arm_tmp.rect.width/(arm_tmp.rect.height+0.0001)), arm_tmp.center + Point_<int>(offset_roi_point) , FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255,255,255), 1);
+#endif
+
+                if(arm_tmp.is_suitable_size())
+                {
+                    if(arm_tmp.get_average_intensity(gray)< 255 )
+                    {
+                        arm_tmp.max_match(LED_Stick_v, i, j);
+                    }
+                }
+            }
+        }
+    }
+
+    // 分类装甲板 根据灯条匹配状态得到最终装甲板
+    vector<armor> final_armor_list;
+    for(size_t i = 0; i < LED_Stick_v.size() ; i++)
+    {
+        if(LED_Stick_v.at(i).matched)
+        {
+            LED_Stick_v.at(LED_Stick_v.at(i).match_index).matched = false; //清除之前与之匹配的灯条序号
+            armor arm_tmp( LED_Stick_v.at(i), LED_Stick_v.at(LED_Stick_v.at(i).match_index));
+            final_armor_list.push_back(arm_tmp);
+        }
+    }
+
+    // **选择装甲板** -根据距离图像中心最短选择
+    float dist=1e8;
+    bool found_flag = false;
+    armor target;
+    Point2f roi_center(roi_rect.width/2, roi_rect.height/2);
+    float dx,dy;
+    for (size_t i = 0; i < final_armor_list.size() ; i++ )
+    {
+
+#ifdef FAST_DISTANCE
+        dx = fabs(final_armor_list.at(i).center.x - roi_center.x);
+        dy = fabs(final_armor_list.at(i).center.y - roi_center.y);
+#else
+        dx = pow((final_armor_list.at(i).center.x - roi_center.x), 2.0f);
+        dy = pow((final_armor_list.at(i).center.y - roi_center.y), 2.0f);
+#endif
+
+        if( dx + dy < dist)
+        {
+            target = final_armor_list.at(i);
+            dist = dx + dy;
+        }
+
+#ifdef SHOW_DRAW_RECT
+        final_armor_list.at(i).draw_rect(img, offset_roi_point);
+#endif
+        found_flag = true;
+    }
+
+#ifdef SHOW_ROI_RECTANGLE
+    rectangle(img, roi_rect,Scalar(255, 0, 255),1);
+#endif
+
+    // **计算装甲板四个点顶点** -用于pnp姿态结算
+    RotatedRect target_rect;
+    if(found_flag)
+    {
+
+#ifdef SHOW_DRAW_SPOT
+        target.draw_spot(img, offset_roi_point);
+#endif
+
+        Point2f point_tmp[4];
+        Point2f point_2d[4];
+
+        // 左右灯条分类，分别提取装甲板四个外角点
+        RotatedRect R, L;
+        if(target.Led_stick[0].rect.center.x > target.Led_stick[1].rect.center.x)
+        {
+            R = target.Led_stick[0].rect;
+            L = target.Led_stick[1].rect;
+        }else
+        {
+            R = target.Led_stick[1].rect;
+            L = target.Led_stick[0].rect;
+        }
+
+        L.points(point_tmp);
+        point_2d[0] = point_tmp[1];
+        point_2d[3] = point_tmp[0];
+        R.points(point_tmp);
+        point_2d[1] = point_tmp[2];
+        point_2d[2] = point_tmp[3];
+
+        points_2d_.clear();
+        vector<Point2f> points_roi_tmp;
+        for(int i=0;i<4;i++)
+        {
+            points_roi_tmp.push_back(point_2d[i]);
+            points_2d_.push_back(point_2d[i] + offset_roi_point);
+        }
+
+        //计算ROI的相关参数
+        last_target_ = boundingRect(points_roi_tmp);
+        rectangle(img, last_target_,Scalar(255,255,255), 1);
+        lost_cnt_ = 0;
+    }
+    else
+    {
+        //计算ROI的相关参数
+        lost_cnt_ ++;
+    }
+
+    detect_cnt_++;
+    return found_flag;
+}
